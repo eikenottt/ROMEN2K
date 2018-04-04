d5e11817fb279130bf8fa80edf73fc250f9043ed
IDEA-PC\JohnTore <Jtsimonsen92@gmail.com>
2018-04-04 18:44:38 +0200

    reviewProcessor.py lagt til, tanke for klassen er å håndtere review txt filer å gjøre dem klar til å brukes i naivebayesclassifier

---
+++ reviewProcessor.py
@@ -0,0 +1,8 @@
+
+testString = "Good movies are good, this one was a good movie"
+testString2 = "Worst movie, ever"
+testString3 = "Okay movie, horrible story, good acting"
+testString4 = "Give this man an Oscar"
+testString5 = "bad movie"
+
+reviews[]
