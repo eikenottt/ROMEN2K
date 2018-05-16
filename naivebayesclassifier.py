2e533e7f7be9ca1d9d27569e470a0aab19816738
runeeikemo <rune.eikem@gmail.com>
2018-05-16 08:55:34 +0200

    Added pseudocode and some other code

--- naivebayesclassifier.py
+++ naivebayesclassifier.py
@@ -9,6 +9,7 @@ version: 1.0
 import glob
 from collections import Counter
 import string
+from math import log, sqrt
 
 stopword = {"the", "a", "of", "in", "are", "to", "and", "is", "&", "br", "but", "it",
             "that", "as", "was", "for", "on", "be", "width", "have", "its", "one",
@@ -16,11 +17,25 @@ stopword = {"the", "a", "of", "in", "are", "to", "and", "is", "&", "br", "but", 
 punct = string.punctuation
 i = 0
 
+threshold = 0.1
+
+
+def train_native_bayer(dir, C):
+    posdir = glob.glob(dir+C[0]+"*.txt")
+    negdir = glob.glob(dir+C[1]+"*.txt")
+    ndoc = len(posdir)+len(negdir)
+
+
+    for c in C:
+        dir = glob.glob(dir+c+"*.txt")
+        nc = len(dir)
+        logprior = nc/ndoc
+
 
 def make_vocab(dir):
     global arr, i
     for fle in dir:
-        if i > 500:
+        if i > 50:
             break
         with open(fle, "r") as rtxt:
             rtxt = rtxt.read().replace('\n', '')
@@ -40,13 +55,64 @@ def make_vocab(dir):
     return arr
 
 
-arr = Counter()
+def compare_dict(dict1, dict2):
+    for i in list(dict1):
+        for j in list(dict2):
+            if i == j:
+                diff = differens(dict1[i], dict2[j])
+                if diff <= threshold:
+                    stopword.add(i)
+                    dict1.pop(i)
+                    dict2.pop(j)
+                    break
 
-negFiles = glob.glob("train/neg/*.txt")
-neg = make_vocab(negFiles)
 
-arr = Counter()
-posFiles = glob.glob("train/pos/*.txt")
-pos = make_vocab(posFiles)
+def compare_input(pos, neg, input):
+    # split input
+    # diffarray = []
+    # make variables for pos and neg values
 
-print("Positive words: {} \nNegative words: {}".format(pos, neg))
+    # loop input[i] alias key
+        # check if in stopwords
+            # pop word
+            # continue
+
+        # pos find value on key
+            # if value exists
+                # save value as possave
+            # else
+                # value is 0
+
+        # neg find value on key
+            # if value exists
+                # save value as negsave
+            # else
+                # value is 0
+
+        # calculate the difference between possave and negsave
+        # place difference in array
+
+    #run naive_bayes function
+
+    return
+
+
+def differens(x, y):
+    diff = x - y
+    return diff / (x + y)
+
+
+# arr = Counter()
+#
+# negFiles = "train/"
+# C =["pos/", "neg/"]
+# neg = make_vocab(negFiles)
+
+# arr = Counter()
+# posFiles = glob.glob("train/pos/*.txt")
+# pos = make_vocab(posFiles)
+
+# compare_dict(pos, neg)
+
+# print("Positive words: {} \nNegative words: {}\n".format(pos, neg))
+# print("Stopword list: {}".format(sorted(stopword)))
--- reviewProcessor.py
+++ reviewProcessor.py
@@ -1,8 +1,7 @@
-
 testString = "Good movies are good, this one was a good movie"
 testString2 = "Worst movie, ever"
 testString3 = "Okay movie, horrible story, good acting"
 testString4 = "Give this man an Oscar"
 testString5 = "bad movie"
 
-reviews[] =
+reviews = [testString, testString2, testString3, testString4, testString5]
