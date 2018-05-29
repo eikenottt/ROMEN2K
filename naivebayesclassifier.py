fd33fcaff71dba84e7b497330453c8c8ee05ba47
IDEA-PC\JohnTore <Jtsimonsen92@gmail.com>
2018-05-29 21:38:42 +0200

    Fikset kommetarer + endring av variabel navn- med mer.

--- naivebayesclassifier.py
+++ naivebayesclassifier.py
@@ -9,6 +9,7 @@ version: 1.0
 import glob
 from collections import Counter
 import string
+import os
 from math import log, sqrt
 
 stopword = {"the", "a", "of", "in", "are", "to", "and", "is", "&", "br", "but", "it",
@@ -22,23 +23,22 @@ threshold = 0.1
 
 
 def train_native_bayer(dir, C):
-    posdir = glob.glob(dir+C[0]+"*.txt")
-    negdir = glob.glob(dir+C[1]+"*.txt")
-    ndoc = len(posdir)+len(negdir)
-
+    posdir = glob.glob(dir + C[0] + "*.txt")
+    negdir = glob.glob(dir + C[1] + "*.txt")
+    ndoc = len(posdir) + len(negdir)
 
     for c in C:
-        dir = glob.glob(dir+c+"*.txt")
+        dir = glob.glob(dir + c + "*.txt")
         nc = len(dir)
-        logprior = nc/ndoc
+        logprior = nc / ndoc
 
 
 def make_vocab(dir):
     global arr, i
     for fle in dir:
-        if i > 500:
+        if i > 200:
             break
-        with open(fle, "r") as rtxt:
+        with open(fle, "r", encoding="utf8") as rtxt:
             rtxt = rtxt.read().replace('\n', '')
 
             rtxt = rtxt.lower()
@@ -59,7 +59,7 @@ def compare_dict(dict1, dict2):
     for i in list(dict1):
         for j in list(dict2):
             if i == j:
-                diff = differens(dict1[i], dict2[j])
+                diff = difference(dict1[i], dict2[j])
                 if diff <= threshold:
                     stopword.add(i)
                     dict1.pop(i)
@@ -73,62 +73,74 @@ def compare_input(pos, neg, input):
     # make variables for pos and neg values
 
     # loop input[i] alias key
-        # check if in stopwords
-            # pop word
-            # continue
+    # check if in stopwords
+    # pop word
+    # continue
 
-        # pos find value on key
-            # if value exists
-                # save value as possave
-            # else
-                # value is 0
+    # pos find value on key
+    # if value exists
+    # save value as possave
+    # else
+    # value is 0
 
-        # neg find value on key
-            # if value exists
-                # save value as negsave
-            # else
-                # value is 0
+    # neg find value on key
+    # if value exists
+    # save value as negsave
+    # else
+    # value is 0
 
-        # calculate the difference between possave and negsave
-        # place difference in array
+    # calculate the difference between possave and negsave
+    # place difference in array
 
-    #run naive_bayes function
+    # run naive_bayes function
 
     return
 
-def naive_bayes(w, c):
-    allvocab = pos + neg
-    result = 1
-    if allvocab[w] != 0:
-        allvalues = sum(allvocab.values())
-        wordclass = allvocab[w]
 
-        winpos = c[w] / sum(c.values())  # P(w|c) w = word, c = positive || negative
+def naive_bayes(word, category):
+    """
+    Calculating the probability of a given word to occur in a category (positive/negative)
+    :param word: A word
+    :param category: The class positive or negative
+    :return: the probably of word occurring in the category
+    """
+    vocabulary = pos + neg  # Every word found in both positive and negative dictionary
+    result = 1  # the result value is set to 1 to prevent dividing by zero.
+    if vocabulary[word] != 0:
+        allvalues = sum(vocabulary.values())  # the integer sum of all words occurring in the vocubalary
+        word_occurrence = vocabulary[word]  # the integer of occurrences of the word
 
-        if winpos != 0:
-            poslike = sum(c.values()) / allvalues  # P(c) c = positive || negative
-            wordlike = wordclass / allvalues  # P(w) w = word
-            result = (winpos) * (poslike / wordlike)
+        word_in_class = category[word] / sum(
+            category.values())  # P(w|c) w = word, c = positive || negative (Conditional probability)
+
+        if word_in_class != 0:  # If word doesn't occur in the class
+            class_prob = sum(category.values()) / allvalues  # P(c) c = positive || negative
+            word_prob = word_occurrence / allvalues  # P(w) w = word
+            result = word_in_class * (
+                    class_prob / word_prob)  # Calculating the probability of the word occurring in class
 
     return result
 
 
 def likelihood(sentence):
-    sentence = sentence.translate(punct)
-    sen = sentence.lower().split(" ")
-    s = Counter(sen)
-    sumpo = sumne = 1
-    for w in s.items():
-        po = naive_bayes(w[0], pos)
-        sumpo *= po
-        ne = naive_bayes(w[0], neg)
-        sumne *= ne
-    return sumpo, sumne
+    """
+    Calculates probaility of a sentence/review is positive or negative.
+    :param sentence: sentence or revies to be categorized
+    :return: probility of sentence belonging to category positive or negative
+    """
+    sentence = sentence.translate(punct)  # Removes all the punctuations from given sentence
+    word_arr = sentence.lower().split(" ")  # All words to lower-case and split words to array
+    frequence_of_words = Counter(word_arr)  # Notes the frequency/occurrence of each word in sentence
+    prob_positive = prob_negative = 1  # Sets sum of positive and negative to 1, to prevent dividing by 0 and accumulate
+    for w in frequence_of_words.items():  # Loops through words to calculate possibilities
+        prob_word_positive = naive_bayes(w[0], pos)  # Calculates probability of word occurring in positive review
+        prob_positive *= prob_word_positive  # Multiplies the probabilities together to find probability of whole sentence
+        prob_word_negative = naive_bayes(w[0], neg)  # Repeat same process for negative.
+        prob_negative *= prob_word_negative
+    return prob_positive, prob_negative  # return both probabilities
 
 
-
-
-def differens(x, y):
+def difference(x, y):
     diff = x - y
     return diff / (x + y)
 
@@ -142,13 +154,20 @@ arr = Counter()
 posFiles = glob.glob("train/pos/*.txt")
 pos = make_vocab(posFiles)
 
-positive, negative = likelihood("bad,")
+test_sentence = 'good good good good '
+
+
+test_sentence = test_sentence.lower()
+test_sentence = test_sentence.translate(punct)
+
+positive, negative = likelihood(test_sentence)
 
 print(neg['bad'])
 
-print("Positive sentence: {0:.2f} \nNegative sentence: {1:.2f} ".format(positive, negative))
+print("Positive sentence: {} \nNegative sentence: {} ".format(positive, negative))
+# dir = ["neg", "pos"]
+# print(os.getcwd() + "/train" + "/" + dir[0] + "/*.txt")
 
 # compare_dict(pos, neg)
-
 # print("Positive words: {} \nNegative words: {}\n".format(pos, neg))
 # print("Stopword list: {}".format(sorted(stopword)))
