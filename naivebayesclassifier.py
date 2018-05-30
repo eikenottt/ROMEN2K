1baec5bae15c7d00e43ab2ef95c79ba73d2555a0
runeeikemo <rune.eikem@gmail.com>
2018-05-30 15:45:55 +0200

    Optimaliserte fra 30 sek per review til 5 sek per 10. review

--- naivebayesclassifier.py
+++ naivebayesclassifier.py
@@ -7,32 +7,23 @@ version: 1.0
 """
 
 import glob
+import random
 from collections import Counter
 import string
 import pickle
 import os
-from math import log, sqrt
+import re
+
+
+import time
 
 stopword = {"the", "a", "of", "in", "are", "to", "and", "is", "&", "br", "but", "it",
             "that", "as", "was", "for", "on", "be", "width", "have", "its", "one",
             "at", "", "so", "or", "an", "by"}
 punct = str.maketrans(dict.fromkeys(string.punctuation))
 
-threshold = 0.1
 
-
-def train_native_bayer(dir, C):
-    posdir = glob.glob(dir + C[0] + "*.txt")
-    negdir = glob.glob(dir + C[1] + "*.txt")
-    ndoc = len(posdir) + len(negdir)
-
-    for c in C:
-        dir = glob.glob(dir + c + "*.txt")
-        nc = len(dir)
-        logprior = nc / ndoc
-
-
-def make_vocab(directory, limit=500):
+def make_vocab(directory, limit):
     """
     Creates a vocabulary from a directory containing text files
     :param directory:
@@ -60,48 +51,6 @@ def make_vocab(directory, limit=500):
     return arr
 
 
-def compare_dict(dict1, dict2):
-    for i in list(dict1):
-        for j in list(dict2):
-            if i == j:
-                diff = difference(dict1[i], dict2[j])
-                if diff <= threshold:
-                    stopword.add(i)
-                    dict1.pop(i)
-                    dict2.pop(j)
-                    break
-
-
-def compare_input(pos, neg, input):
-    # split input
-    # diffarray = []
-    # make variables for pos and neg values
-
-    # loop input[i] alias key
-    # check if in stopwords
-    # pop word
-    # continue
-
-    # pos find value on key
-    # if value exists
-    # save value as possave
-    # else
-    # value is 0
-
-    # neg find value on key
-    # if value exists
-    # save value as negsave
-    # else
-    # value is 0
-
-    # calculate the difference between possave and negsave
-    # place difference in array
-
-    # run naive_bayes function
-
-    return
-
-
 def naive_bayes(word, category):
     """
     Calculating the probability of a given word to occur in a category (positive/negative)
@@ -109,7 +58,6 @@ def naive_bayes(word, category):
     :param category: The class positive or negative
     :return: the probably of word occurring in the category
     """
-    vocabulary = pos + neg  # Every word found in both positive and negative dictionary
     result = 1  # the result value is set to 1 to prevent dividing by zero.
     if vocabulary[word] != 0:
         all_values = sum(vocabulary.values())  # the integer sum of all words occurring in the vocubalary
@@ -139,6 +87,8 @@ def likelihood(sentence):
     frequence_of_words = Counter(word_arr)  # Notes the frequency/occurrence of each word in sentence
     prob_positive = prob_negative = 1  # Sets sum of positive and negative to 1, to prevent dividing by 0 and accumulate
     for w in frequence_of_words.items():  # Loops through words to calculate possibilities
+        # if vocabulary[w[0]] != 0:
+        #     print(pos[w[0]])
         prob_word_positive = naive_bayes(w[0], pos)  # Calculates probability of word occurring in positive review
         prob_positive *= prob_word_positive  # Multiplies the probabilities together to find probability of whole sentence
         prob_word_negative = naive_bayes(w[0], neg)  # Repeat same process for negative.
@@ -183,42 +133,68 @@ def test_model(test_sentence):
     print(test_sentence[:20], "...")
     print("Positive sentence: {} \nNegative sentence: {} ".format(positive, negative))
     if positive > negative:
-        print("Positive percentage:", (positive/(positive+negative))*100)
+        print("Positive percentage:", (positive / (positive + negative)) * 100)
     else:
-        print("Negative percentage:", (negative/(positive+negative))*100)
+        print("Negative percentage:", (negative / (positive + negative)) * 100)
     print()
-    # dir = ["neg", "pos"]
-    # print(os.getcwd() + "/train" + "/" + dir[0] + "/*.txt")
 
-    # compare_dict(pos, neg)
-    # print("Positive words: {} \nNegative words: {}\n".format(pos, neg))
-    # print("Stopword list: {}".format(sorted(stopword)))
 
-# def add_review(sentence, c):
-#
+def test_large_set_of_reviews(directory):
+    gsl = {'tp': 0, 'fp': 0, 'tn': 0, 'fn': 0}
 
+    test_directory = glob.glob(directory)
+    random.shuffle(test_directory)
+    limit = 100
+    test_directory = test_directory[:limit]
+    i = 1
+    start_time = time.time()
+    for review_path in test_directory:
+        class_label = re.search(r"/(\w+)/", review_path).group(1)
+        with open(review_path, "r", encoding="utf8") as review:
+            key = compare_class_labels(test_single_review(review.read()), class_label)
+            review.close()
+        gsl[key] += 1
+        if i % 10 == 0:
+            print("Time spent on {} reviews, {:.2f} seconds:".format(str(i)+"/"+str(limit), time.time()-start_time))
+        i += 1
+    precision_pos = gsl['tp'] / (gsl['tp']+gsl['fp'])
+    precision_neg = gsl['tn'] / (gsl['tn']+gsl['fn'])
+    recall_pos = gsl['tp'] / (gsl['tp']+gsl['fn'])
+    recall_neg = gsl['tn'] / (gsl['tn']+gsl['fp'])
+    accuracy = (gsl['tp']+gsl['tn']) / (gsl['tp']+gsl['fp']+gsl['tn']+gsl['fn'])
+    print("TP: {}\nTN: {}\nFP: {}\nFN: {}\n".format(gsl['tp'], gsl['tn'], gsl['fp'], gsl['fn']))
+    print("Precision Positive:", precision_pos)
+    print("Precision Negative:", precision_neg)
+    print("Recall Positive:", recall_pos)
+    print("Recall Negative:", recall_neg)
+    print("Accuracy:", accuracy)
+
+
+def test_single_review(review):
+    prob_pos, prob_neg = likelihood(review)
+    if prob_pos > prob_neg:
+        return "pos"
+    else:
+        return "neg"
+
+
+def compare_class_labels(label, real_label):
+    if label == "pos":
+        if label == real_label:
+            return 'tp'
+        else:
+            return 'fp'
+    else:
+        if label == real_label:
+            return 'tn'
+        else:
+            return 'fn'
 
 
 if __name__ == '__main__':
     pos, neg = train_model()
-    negative_review = "Once again Mr. Costner has dragged out a movie for far longer than necessary. Aside from the " \
-                      "terrific sea rescue sequences, of which there are very few I just did not care about any of " \
-                      "the characters. Most of us have ghosts in the closet, and Costner's character are realized " \
-                      "early on, and then forgotten until much later, by which time I did not care. The character we " \
-                      "should really care about is a very cocky, overconfident Ashton Kutcher. The problem is he " \
-                      "comes off as kid who thinks he's better than anyone else around him and shows no signs of a " \
-                      "cluttered closet. His only obstacle appears to be winning over Costner. Finally when we are " \
-                      "well past the half way point of this stinker, Costner tells us all about Kutcher's ghosts. We " \
-                      "are told why Kutcher is driven to be the best with no prior inkling or foreshadowing. No magic " \
-                      "here, it was all I could do to keep from turning it off an hour in. "
-    positive_review = "I went and saw this movie last night after being coaxed to by a few friends of mine. I'll " \
-                      "admit that I was reluctant to see it because from what I knew of Ashton Kutcher he was only " \
-                      "able to do comedy. I was wrong. Kutcher played the character of Jake Fischer very well, " \
-                      "and Kevin Costner played Ben Randall with such professionalism. The sign of a good movie is " \
-                      "that it can toy with our emotions. This one did exactly that. The entire theater (which was " \
-                      "sold out) was overcome by laughter during the first half of the movie, and were moved to tears " \
-                      "during the second half. While exiting the theater I not only saw many women in tears, " \
-                      "but many full grown men as well, trying desperately not to let anyone see them crying. This " \
-                      "movie was great, and I suggest that you go see it before you judge. "
-    test_model(negative_review)
-    test_model(positive_review)
+    vocabulary = pos + neg
+    start_time = time.time()
+    test_large_set_of_reviews("test/*/*.txt")
+    print("Time spent in total {:.2f} seconds".format((time.time() - start_time)))
+
