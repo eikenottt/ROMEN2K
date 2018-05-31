ec566920da5e98b1552312a15532b02ffd334fb3
runeeikemo <rune.eikem@gmail.com>
2018-05-31 11:06:53 +0200

    har fiksa treig trening i make vocab, Stopwords blir generert ut fra vocabularet, Har lagt til error rate

--- naivebayesclassifier.py
+++ naivebayesclassifier.py
@@ -16,13 +16,24 @@ import re
 import numpy
 import math
 
-
 import time
 
-stopword = {"the", "a", "of", "in", "are", "to", "and", "is", "&", "br", "but", "it",
-            "that", "as", "was", "for", "on", "be", "width", "have", "its", "one",
-            "at", "", "so", "or", "an", "by"}
+
 punct = str.maketrans(dict.fromkeys(string.punctuation))
+
+
+def make_stopword_list(vocab):
+    stopword = []
+    for n in vocab:
+        word_occ = vocab[n]
+        num_of_times_in = (word_occ / len(vocab)) * 100
+        if num_of_times_in > 1:
+            stopword.append(n)
+
+    for e in stopword:
+        vocab.pop(e)
+
+    return stopword
 
 
 def make_vocab(directory, limit):
@@ -38,17 +49,8 @@ def make_vocab(directory, limit):
         if i > limit:
             break
         with open(fle, "r", encoding="utf8") as rtxt:
-            rtxt = rtxt.read().replace('\n', '')
-
-            rtxt = rtxt.lower()
-
-            rtxt = rtxt.translate(punct)
-
-            rtxt = rtxt.split(" ")
-            okarray = [s for s in rtxt if s not in stopword]
-
-            temp = Counter(okarray)
-            arr = arr + temp
+            rtxt = rtxt.read().replace('\n', '').lower().translate(punct).split(" ")
+            arr.update(rtxt)
         i = i + 1
     return arr
 
@@ -91,8 +93,6 @@ def likelihood(sentence):
     frequence_of_words = Counter(word_arr)  # Notes the frequency/occurrence of each word in sentence
     prob_positive = prob_negative = 1  # Sets sum of positive and negative to 1, to prevent dividing by 0 and accumulate
     for w in frequence_of_words.items():  # Loops through words to calculate possibilities
-    #     if vocabulary[w[0]] != 0:
-    #         print(pos[w[0]])
         prob_word_positive = naive_bayes(w[0], pos)  # Calculates probability of word occurring in positive review
         prob_positive = prob_positive * prob_word_positive  # Multiplies the probabilities together to find probability of whole sentence
         prob_word_negative = naive_bayes(w[0], neg)  # Repeat same process for negative.
@@ -114,7 +114,11 @@ def train_model(filename='trained.model', limit=12500):
         pos_files = glob.glob("train/pos/*.txt")
         pos_vocab = make_vocab(pos_files, limit)
 
-        model_dict = {'pos': pos_vocab, 'neg': neg_vocab}
+        all_vocab = neg_vocab + pos_vocab
+
+        stopword = make_stopword_list(all_vocab)
+
+        model_dict = {'pos': pos_vocab, 'neg': neg_vocab, 'stopwords': stopword}
         save_file = open(filename, 'wb')
         pickle.dump(model_dict, save_file)
         save_file.close()
@@ -124,7 +128,8 @@ def train_model(filename='trained.model', limit=12500):
         get_file.close()
         pos_vocab = model_dict['pos']
         neg_vocab = model_dict['neg']
-    return pos_vocab, neg_vocab
+        stopword = model_dict['stopwords']
+    return pos_vocab, neg_vocab, stopword
 
 
 def test_model(test_sentence):
@@ -143,9 +148,10 @@ def test_large_set_of_reviews(directory):
 
     test_directory = glob.glob(directory)
     random.shuffle(test_directory)
-    limit = 20
+    limit = 30
     test_directory = test_directory[:limit]
     i = 1
+    beta = 1
     regex = re.compile('[\\\/](\w+)[\\\/]')
     start_time = time.time()
     for review_path in test_directory:
@@ -155,19 +161,25 @@ def test_large_set_of_reviews(directory):
             review.close()
         gsl[key] += 1
         if i % 10 == 0:
-            print("Time spent on {} reviews, {:.2f} seconds:".format(str(i)+"/"+str(limit), time.time()-start_time))
+            print(
+                "Time spent on {} reviews, {:.2f} seconds:".format(str(i) + "/" + str(limit), time.time() - start_time))
         i += 1
-    precision_pos = gsl['tp'] / (gsl['tp']+gsl['fp'])
-    precision_neg = gsl['tn'] / (gsl['tn']+gsl['fn'])
-    recall_pos = gsl['tp'] / (gsl['tp']+gsl['fn'])
-    recall_neg = gsl['tn'] / (gsl['tn']+gsl['fp'])
-    accuracy = (gsl['tp']+gsl['tn']) / (gsl['tp']+gsl['fp']+gsl['tn']+gsl['fn'])
+    precision_pos = gsl['tp'] / (gsl['tp'] + gsl['fp'])
+    precision_neg = gsl['tn'] / (gsl['tn'] + gsl['fn'])
+    recall_pos = gsl['tp'] / (gsl['tp'] + gsl['fn'])
+    recall_neg = gsl['tn'] / (gsl['tn'] + gsl['fp'])
+    f_mesure = ((beta ** 2 + 1) * precision_pos * recall_pos) / (precision_pos + recall_pos)
+    accuracy = (gsl['tp'] + gsl['tn']) / (gsl['tp'] + gsl['fp'] + gsl['tn'] + gsl['fn'])
+    error_rate = 1 - f_mesure
+
     print("TP: {}\nTN: {}\nFP: {}\nFN: {}\n".format(gsl['tp'], gsl['tn'], gsl['fp'], gsl['fn']))
     print("Precision Positive:", precision_pos)
     print("Precision Negative:", precision_neg)
     print("Recall Positive:", recall_pos)
     print("Recall Negative:", recall_neg)
     print("Accuracy:", accuracy)
+    print("Error Rate:", error_rate)
+    print("F-Measure:", f_mesure)
 
 
 def test_single_review(review):
@@ -192,7 +204,7 @@ def compare_class_labels(label, real_label):
 
 
 if __name__ == '__main__':
-    pos, neg = train_model()
+    pos, neg, stopword = train_model()
     pos_values = sum(pos.values())  # P(w|c) w = word, c = positive || negative (Conditional probability)
     neg_values = sum(neg.values())  # P(w|c) w = word, c = positive || negative (Conditional probability)
     vocabulary = pos + neg
@@ -200,4 +212,3 @@ if __name__ == '__main__':
     start_time = time.time()
     test_large_set_of_reviews("test/*/*.txt")
     print("Time spent in total {:.2f} seconds".format((time.time() - start_time)))
-
